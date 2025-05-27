ed2879e16858228811a6270f1f311c4c41ac87b8
abel <ababcantudo@gmail.com>
2025-05-27 20:28:54 +0200

    change vite configuracion

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,24 @@
-import { defineConfig } from 'vite';
+// vite.config.js
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: './', // correcto para repos tipo *.github.io
-});
-// 
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        quienes_somos: 'quienes_somos.html',
+        blog: 'blog.html',
+        contact: 'contacto.html',
+        curso1:'curso1.html',
+        curso2:'curso2.html',
+        login:'login.html',
+        noticia1:'noticia1.html',
+        noticia2:'noticia2.html',
+        registro:'registro.html',
+        legal:'aviso_legal.html'
+        // Añade aquí todos los html que quieras incluir
+      }
+    }
+  },
+  base: '/conquer-microsite.github.io/'
+})
